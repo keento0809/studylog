c4bea5a2cd4486366dd17663057c519ec5518528
KEENTO <handball0321@gmail.com>
2022-05-02 13:13:08 -0700

    Add NotFound.tsx

--- src/pages/Logs.tsx
+++ src/pages/Logs.tsx
@@ -5,9 +5,18 @@ import LogCard from "../components/UI/Card/LogCard";
 const Logs = () => {
   return (
     <Layout>
-      <div className="">
-        <p>I'm going to add logs here.</p>
-        <LogCard />
+      <div className="text-center">
+        <div className="py-3">
+          <h1 className="text-3xl font-semibold text-gray-800 dark:text-gray-100">
+            Log History
+          </h1>
+        </div>
+        {/* temporary: need to set max-height and overflow: scroll */}
+        <div className="">
+          {/* I'm gonna add logList here. */}
+          <LogCard />
+          <LogCard />
+        </div>
       </div>
     </Layout>
   );
