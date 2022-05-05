431c90a31c18210b13dcd344328a78bc63dcadfb
KEENTO <handball0321@gmail.com>
2022-05-05 16:09:44 -0700

    Add icon to toggle dark mode in Hero.tsx

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,12 +1,18 @@
-import { Fragment, useEffect } from "react";
+import { Fragment, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import Footer from "../layouts/Footer";
 import HeroModal from "../components/UI/Modal/HeroModal";
+import LightModeContext from "../contexts/lightmode-context";
 
 const Hero = () => {
-  useEffect(() => {
-    window.document.documentElement.classList.add("dark");
-  }, []);
+  // declare useContext
+  const lightModeCtx = useContext<any>(LightModeContext);
+
+  const handleToggleMode = () => {
+    lightModeCtx.toggleMode();
+    console.log(lightModeCtx.isLightMode);
+    window.document.documentElement.classList.toggle("dark");
+  };
 
   return (
     <Fragment>
@@ -14,7 +20,7 @@ const Hero = () => {
       <header className="bg-white dark:bg-gray-800 min-h-screen">
         <nav className="">
           <div className="container flex items-center justify-between px-6 py-3 mx-auto">
-            <div>
+            <div className="flex flex-row items-center">
               <Link
                 className="text-2xl font-bold text-gray-800 dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300"
                 // href="#"
@@ -22,6 +28,40 @@ const Hero = () => {
               >
                 StudyLog
               </Link>
+              {lightModeCtx.isLightMode && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="sun inline-block ml-4 h-6 w-6 text-gray-700 dark:text-slate-100"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  onClick={handleToggleMode}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                  />
+                </svg>
+              )}
+              {!lightModeCtx.isLightMode && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="moon inline-block ml-4 h-6 w-6 text-gray-700 dark:text-slate-100"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  onClick={handleToggleMode}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                  />
+                </svg>
+              )}
             </div>
 
             <a
