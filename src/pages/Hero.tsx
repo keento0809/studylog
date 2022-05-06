d9d5d90de83c410e6cb70d23a9eb4642b33fd35d
KEENTO <handball0321@gmail.com>
2022-05-06 14:46:40 -0700

    Add mediaquery to Hero.tsx

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -18,8 +18,8 @@ const Hero = () => {
     <Fragment>
       <HeroModal />
       <header className="bg-white dark:bg-gray-800 min-h-screen">
-        <nav className="">
-          <div className="container flex items-center justify-between px-6 py-3 mx-auto">
+        <nav className="relative">
+          <div className="fixedにするよー z-10 sticky top-0 container bg-white dark:bg-gray-800 flex items-center justify-between px-6 py-3 mx-auto">
             <div className="flex flex-row items-center">
               <Link
                 className="text-2xl font-bold text-gray-800 dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300"
@@ -65,7 +65,7 @@ const Hero = () => {
             </div>
 
             <a
-              className="my-1 text-sm font-medium text-emerald-400 dark:text-gray-200 hover:text-emerald-500 dark:hover:text-emerald-500 lg:mx-4 lg:my-0"
+              className="my-1 text-sm font-medium text-emerald-400 dark:text-emerald-500 hover:text-emerald-500 dark:hover:text-emerald-500 lg:mx-4 lg:my-0"
               href="#"
               data-bs-toggle="modal"
               data-bs-target="#heroModal"
@@ -87,8 +87,8 @@ const Hero = () => {
         </nav>
 
         <div className="こいつにflexboxつけるよ">
-          <div className="container px-6 py-16 mx-auto">
-            <div className="items-center lg:flex">
+          <div className="container md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl flex justify-center md:justify-start items-start px-6 pt-14 md:pt-8 lg:pt-28 mx-auto">
+            <div className="items-center lg:flex grow">
               <div className="w-full lg:w-1/2">
                 <div className="lg:max-w-lg">
                   <h1 className="text-2xl font-semibold text-gray-800 dark:text-white lg:text-3xl">
@@ -103,16 +103,16 @@ const Hero = () => {
                   </p>
 
                   <div className="flex flex-col mt-8 space-y-3 lg:space-y-0 lg:flex-row">
-                    <button className="w-full md:w-40 md:mr-auto px-4 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-emerald-400 dark:bg-emerald-500 rounded-full lg:w-auto lg:mx-4 hover:bg-emerald-500 focus:outline-none focus:bg-emerald-500">
+                    <button className="w-full md:w-40 md:mr-auto lg:mx-0 px-4 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-emerald-400 dark:bg-emerald-500 rounded-full lg:w-auto hover:bg-emerald-500 focus:outline-none focus:bg-emerald-500">
                       <Link to="/home">Get started</Link>
                     </button>
                   </div>
                 </div>
               </div>
 
-              <div className="flex items-center justify-center md:justify-start w-full mt-6 lg:mt-0 lg:w-1/2 rounded-lg">
+              <div className="flex grow items-center justify-center md:justify-start w-full mt-6 lg:mt-0 lg:w-1/2 rounded-lg">
                 <img
-                  className="w-full h-full max-w-md rounded-lg"
+                  className="w-full h-full max-w-md lg:max-w-none rounded-lg"
                   src="https://images.unsplash.com/photo-1471107340929-a87cd0f5b5f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2873&q=80"
                   alt="#"
                 />
