3566e5ffcfb0b6698426156bd872de935a3bbf54
KEENTO <handball0321@gmail.com>
2022-05-07 14:46:06 -0700

    Refactor MainComponent

---
+++ src/components/Log/HomeHistory.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import LogList from "./LogList";
+
+const HomeHistory = () => {
+  return (
+    <div className="hidden lg:block ">
+      <h1 className="text-2xl font-semibold text-gray-800 dark:text-gray-100">
+        History
+      </h1>
+      <div className="mt-4 max-h-48 overflow-hidden">
+        <LogList />
+      </div>
+      <button className="w-1/4 mx-autos my-3 px-2 py-1 text-sm font-medium tracking-wide text-emerald-400 border border-emerald-400 capitalize transition-colors duration-200 transform bg-white dark:bg-gray-800 rounded-full sm:mx-2 hover:bg-emerald-300 focus:outline-none focus:bg-emerald-500">
+        <Link to="/history">More</Link>
+      </button>
+    </div>
+  );
+};
+
+export default HomeHistory;
--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 import HomeHourChart from "./UI/Chart/HomeHourChart";
 import SuccessAlert from "./UI/Alert/SuccessAlert";
 import HomeCostChart from "./UI/Chart/HomeCostChart";
-import LogList from "./Log/LogList";
-import HomeSectionCard from "./UI/Card/HomeSectionCard";
+import HomeHistory from "./Log/HomeHistory";
+import HomeAnalysis from "./Analysis/HomeAnalysis";
 
 const MainComponent = () => {
   const [isAlert, setIsAlert] = React.useState(false);
@@ -15,30 +15,8 @@ const MainComponent = () => {
     <div className="mx-auto lg:flex lg:flex-row md:max-w-screen-md lg:max-w-screen-lg">
       <AddLogForm setIsAlert={setIsAlert} />
       <div className="text-center py-4 lg:py-8 mx-auto md:max-w-lg lg:w-full">
-        {/* I gutta replace this section to HomeHistory component */}
-        <h1 className="text-3xl lg:text-2xl font-semibold text-gray-800 dark:text-gray-100">
-          Analysis
-        </h1>
-        <div className="pt-8 lg:pb-8 lg:flex lg:flex-row">
-          <div className="mx-auto lg:w-1/2">
-            <HomeHourChart />
-          </div>
-          <div className="mx-auto lg:w-1/2">
-            <HomeCostChart />
-          </div>
-        </div>
-        {/* I gutta replace this section to HomeHistory component */}
-        <div className="hidden lg:block ">
-          <h1 className="text-2xl font-semibold text-gray-800 dark:text-gray-100">
-            History
-          </h1>
-          <div className="mt-5 max-h-48 overflow-hidden">
-            <LogList />
-          </div>
-          <button className="w-1/4 mx-autos my-3 px-2 py-1 text-sm font-medium tracking-wide text-emerald-400 border border-emerald-400 capitalize transition-colors duration-200 transform bg-white dark:bg-gray-800 rounded-full sm:mx-2 hover:bg-emerald-300 focus:outline-none focus:bg-emerald-500">
-            <Link to="/history">More</Link>
-          </button>
-        </div>
+        <HomeAnalysis />
+        <HomeHistory />
       </div>
       {isAlert && <SuccessAlert isAlert={isAlert} />}
     </div>
